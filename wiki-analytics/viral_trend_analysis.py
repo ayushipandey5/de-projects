836b130c7bce60f4c1a05fd6ff152a9e0a98719d
Ayushi Pandey <ayuup198@gmail.com>
2026-07-06 13:03:38 +0000

    fixed window trending domain beam stream

--- wiki-analytics/viral_trend_analysis.py
+++ wiki-analytics/viral_trend_analysis.py
@@ -18,12 +18,21 @@ class AssignEventWatermark(beam.DoFn):
                 yield beam.window.TimestampedValue(data, dt_unix)
         except Exception as e:
             logging.error(f"Error parsing timestamp: {e}")
+    
+    def format_article_for_bq(element):
+        title_url, count = element
+        return {
+            'title_url': title_url,
+            'edit_count': count,
+            'processing_time': datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+        }
 
 def run():
     parser = argparse.ArgumentParser()
     parser.add_argument('--project',required=True,help='GCP Project Id')
     parser.add_argument('--subscription',required=True,help='subscription_id')
     parser.add_argument('--bigq_dataset',required=True,help='big query dataset')
+    parser.add_argument('--tableName',required=True,help="BigQuery Table")
 
     known_args,pipeline_args = parser.parse_known_args
 
@@ -35,6 +44,21 @@ def run():
             p
             | "Read from Pub/Sub" >> beam.io.ReadFromPubSub(subscription=known_args.subscription)
             | "Assign Event Time Watermark" >> beam.Pardo(AssignEventWatermark())
+        )
+
+        # branch 1 - Trending Domains (1-Minute Fixed Window)
+        trending_domains = (
+            parsed_stream
+            | "Branch1 : Extract Server Domain" >> beam.Map(lambda x: (x.get("server_name","unknown"),1))
+            | "Branch1 : 1-Min Fixed Window" >> beam.WindowInto(window.FixedWindows(60))
+            | "Branch1 : Count Per Domain" >> beam.CombinePerkey(sum)
+            | "Branch1 : Format for Bq" >> beam.Map(format_article_for_bq)
+            | "Barch1 : Write to BigQuery" >> beam.io.WriteToBigQuery(
+                table=f"{known_args.project}:{known_args.bigq_dataset}.{known_args.tableName}",
+                schema='server_name:STRING, edit_count:INTEGER, processing_time:TIMESTAMP',
+                write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND,
+                create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED
+            )
         )
 
 if __name__ == "__main__":
